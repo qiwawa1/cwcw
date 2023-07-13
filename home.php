b65abe04c448b28c1d228a95a27d3d0a944b27f8
qiwawa1 <139442548+qiwawa1@users.noreply.github.com>
2023-07-13 16:29:21 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+            ul{
+                background-color: grey;
+                text-align:left;
+            }
+            .li1{
+                text-align: center;
+                color :aliceblue;
+                
+                display:inline;
+            }
+        </style>
+    </head>
+    <body>
+        <ul>
+            <li class="li1"><a href="home.php"><img src ="companylogo.png" ></a></li>
+            <li class="li1"><a href="movie.php">Movies</a></li>
+            <li class="li1"><a href="location.php">Location</a></li>
+            <li class="li1"><a href="login.php">Sign in</a></li>
+            
+        </ul>
+            
+
+       
+    </body>
+</html>
